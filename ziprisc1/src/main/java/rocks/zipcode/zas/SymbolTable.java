fe2be6541e70d22bba5e9a48d754cd9b443cf941
Kristofer <kris@youngers.org>
2020-12-31 11:43:15 -0500

    zas 1

---
+++ ziprisc1/src/main/java/rocks/zipcode/zas/SymbolTable.java
@@ -0,0 +1,7 @@
+package rocks.zipcode.zas;
+
+import java.util.HashMap;
+
+public class SymbolTable extends HashMap<String, String> {
+    // we'll see what I need to add, if anything
+}
---
+++ ziprisc1/src/main/java/rocks/zipcode/zas/ZAS.java
@@ -0,0 +1,38 @@
+package rocks.zipcode.zas;
+
+import java.util.ArrayList;
+import rocks.zipcode.Word;
+
+/*
+ * the ZipRISC1 assembler.
+ */
+
+public class ZAS {
+    ArrayList<Word> instructions = new ArrayList<>();
+    SymbolTable symbols = new SymbolTable();
+    
+    public static void main(String[] args) {
+        ZAS zas = new ZAS();
+        System.err.println("**** ZipRISC1 Assembler v1 ****\n");
+        // System.err.print("args ");
+        // for (String arg : args) {
+        //     System.out.print(" ");
+        //     System.out.print(arg);
+        // }
+        System.err.println();
+        if (args.length == 1) {
+            try {
+                zas.parseFile(args[0]); 
+            } catch (Panic e) {
+                e.printStackTrace();
+                System.err.println(e);
+                java.lang.System.exit(-1);
+            }    
+        } else {
+            System.err.println("no input file.");
+            java.lang.System.exit(-1);
+        }
+        java.lang.System.exit(0);
+    }
+
+}
